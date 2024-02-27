b7f82cded77aaf9eb121e9d3784e3bdd42619c62
amsowmya <soumya.pdit@gmail.com>
2024-02-27 23:06:55 +0530

    modified and added

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ HYPEN_E_DOT = '-e .'
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 REPO_NAME = "mongodb_connector_pkg"
-PKG_NAME = "MongoDB-Connector"
+PKG_NAME = "mongodbautoconnect"
 AUTHOR_USER_NAME = "amsowmya"
 AUTHOR_EMAIL = "sowmya.anekonda@gmail.com"
 
@@ -33,9 +33,9 @@ setup(
     long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
-        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME/{REPO_NAME}/issues}",
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires=get_requiremet("requirements_dev.txt")
+    # install_requires=get_requirement("requirements_dev.txt")
 )
